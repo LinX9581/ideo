196bf4489632e77bb60ef3494d5aab1f17f4a118
lin2 <linx9581@gmail.com>
2018-09-25 21:31:34 +0800

    normal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 
 app.use(express.static('public'));  //靜態檔案放置區
 
@@ -7,6 +8,9 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 }); //網頁起始目錄
 
-app.listen(3001,function(){
-	console.log('listening on *:3001');
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+http.listen(port, host, function() {
+  console.log("Server started.......");
 });
